b5e7e0746f3bd51a1e07a3f36fa860327ae8b1a5
Dave Kelsey <David.Kelsey@gov.bc.ca>
2018-07-17 13:27:07 -0700

    add install of jdk and jenkins

--- provision_jenkins.sh
+++ provision_jenkins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#:<<COMMENT
+:<<COMMENT
+#TODO: get this working
 
 # Install java
 sudo add-apt-repository ppa:webupd8team/java
@@ -11,4 +12,27 @@ echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-se
 apt-get install --yes oracle-java8-installer
 yes "" | apt-get -f install
 
-#COMMENT
+COMMENT
+
+# 
+# Install default-jdk
+#
+
+sudo apt-get install default-jdk -y
+
+
+#
+# Install Jenkins
+#
+
+# Add repo key to system
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+
+# Append the debian package repo address to the sources.list
+echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+
+# Update
+sudo apt-get update
+
+# Install
+sudo apt-get install jenkins -y
